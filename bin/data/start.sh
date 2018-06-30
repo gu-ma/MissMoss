a41471d2f3412d7295488c0404d5ed3ccde455cf
guillaume <4388440+gu-ma@users.noreply.github.com>
2018-06-30 16:39:06 +0200

    Added some misc scripts

---
+++ bin/data/start.sh
@@ -0,0 +1,4 @@
+open '/Users/guillaume/Documents/Dropbox/Exchange_fhnw/OSX/OF/ESP.app'
+cd '/Users/guillaume/Documents/Dropbox/Exchange_fhnw/UBT/featurescoop/featurescoop'
+source activate tf
+python api.py
